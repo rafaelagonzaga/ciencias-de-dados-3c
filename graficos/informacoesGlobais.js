adc1570b05fcd0ad103595fb1cec754952e7bc9e
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2024-10-17 08:00:15 -0300

    il8i9liuo

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,6 +5,11 @@ async function visualizarInformacoesGlobais() {
         const res = await fetch(url)
 
         const dados = await res.json()
+        const pessoasConectadas =(dados.total_pessoas_conectadas) / 1e9
+        const paragrafo = document.createElement('p')
+        paragrafo.classList.add('graficos-container_texto')
+        paragrafo.innerHTML = 'Voce sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximandamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em media
+        <span>${dados.tempo_medio}</span> horas conectadas'
 
         console.log(dados);
 
