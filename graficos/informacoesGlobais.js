f50b19f92cfbc3a84f7dc61e0408544dff497e6a
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2024-10-24 08:19:02 -0300

    il

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -18,3 +18,7 @@ async function visualizarInformacoesGlobais() {
 
     }
 
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+console.log(paragrafo)
+
+
