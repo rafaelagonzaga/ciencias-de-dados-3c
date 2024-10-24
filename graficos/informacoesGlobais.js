d008551227c652f78e1d92a263826367ec641a07
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2024-10-24 08:29:09 -0300

    gukj

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,9 +6,9 @@ async function visualizarInformacoesGlobais() {
 
         const dados = await res.json()
         const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
-        const paragrafo = document.createElement('p')
+        const pessoasNoMundo = document.createElement('p')
         paragrafo.classList.add('graficos-container_texto')
-        paragrafo.innerHTML = `Voce sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximandamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em media
+        paragrafo.innerHTML = `Voce sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximandamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em media
         <span>${dados.tempo_medio}</span> horas conectadas.`
 
         const container = document.getElementByld( "graficos-container")
